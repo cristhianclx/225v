e9c0393a362037e8bdf42720f73961c9ce1e5967
Cristhian <cristhianclx@gmail.com>
2024-12-15 11:28:23 -0500

    03

--- 03/README.txt
+++ 03/README.txt
@@ -30,3 +30,15 @@ flask --app main db init # init
 flask --app main db migrate # create migration
 flask --app main db upgrade # apply migration
 flask --app main db downgrade # revert migration
+
+# database
+
+flask --app main shell
+
+>>> from main import User, db
+>>> user = User(first_name="cristhian", last_name="cueva", age=33, location="huancayo", country="PE")
+>>> db.session.add(user)
+>>> db.session.commit()
+
+>>> from main import User, db
+>>> User.query.all()
--- 03/main.py
+++ 03/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy.sql import func
@@ -13,25 +13,50 @@ migrate = Migrate(app, db)
 
 
 class User(db.Model):
+
     __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(150))
     last_name = db.Column(db.String(150))
     age = db.Column(db.Integer)
-    # location
-    # country
+    location = db.Column(db.String(50))
+    country = db.Column(db.String(10))
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
     def __repr__(self):
         return "<User: {}>".format(self.id)
 
 
-@app.route("/")
-def index():
-    return {}
+class Message(db.Model):
+
+    __tablename__ = "messages"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.String(255))
+    priority = db.Column(db.String(10))
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    def __repr__(self):
+        return "<Message: {}>".format(self.id)
+
 
 @app.route("/status")
 def status():
     return {
         "status": "ok"
     }
+
+
+@app.route("/")
+def index():
+    return {}
+
+
+@app.route("/users")
+def users():
+    data = User.query.all()
+    return render_template("users.html", items=data)
+
+# /messages
+# /users/add
