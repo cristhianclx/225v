3a7e0858511cca58bba67f2a5480d9a2c768870d
Cristhian <cristhianclx@gmail.com>
2024-12-21 18:41:35 -0500

    05

--- 04/main.py
+++ 04/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from bs4 import BeautifulSoup
 import requests
 
 app = Flask(__name__)
@@ -13,16 +14,51 @@ class IndexResource(Resource):
 
 class PokemonByNameResource(Resource):
     def get(self, name):
-        raw = requests.get("https://pokeapi.co/api/v2/pokemon/pikachu")
+        raw = requests.get("https://pokeapi.co/api/v2/pokemon/{}".format(name))
+        if not raw.ok:
+            return {}, 404
         data = raw.json()
+        abilities = []
+        for x in data["abilities"]:
+            abilities.append(x["ability"]["name"])
+        forms = []
+        for x in data["forms"]:
+            forms.append(x["name"])
         return {
             "name": name,
             "height": data["height"],
             "weight": data["weight"],
-            "abilities": [], # fill this 
-            "forms": [] # fill this
+            "abilities": abilities,
+            "forms": forms
+        }
+
+
+class IPInfoResource(Resource):
+    def get(self):
+        r = requests.get("https://ipinfo.io")
+        r_data = r.json()
+        return {
+            "IP": r_data["ip"],
+        }
+
+
+class ExchangeResource(Resource):
+    def get(self):
+        r = requests.get("https://cuantoestaeldolar.pe/")
+        r_source_code = r.text
+        r_soup = BeautifulSoup(r_source_code, 'html.parser')
+        r_elements = r_soup.find_all("div", class_="ValueQuotation_valueContainer__eH4KL")
+        exchange_sale = r_elements[6].text
+        exchange_buy = r_elements[7].text
+        return {
+            "EUR": {
+                "sale": float(exchange_sale),
+                "buy": float(exchange_buy)
+            }
         }
 
 
 api.add_resource(IndexResource, '/')
 api.add_resource(PokemonByNameResource, '/pokemon/<name>')
+api.add_resource(IPInfoResource, '/IP/')
+api.add_resource(ExchangeResource, '/exchange/EUR/')
---
+++ 05/main.py
@@ -0,0 +1,134 @@
+from flask import Flask, request
+from flask_migrate import Migrate
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///instance.db"
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+api = Api(app)
+
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(150), nullable=False)
+    last_name = db.Column(db.String(150), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    location = db.Column(db.String(50), nullable=True)
+    country = db.Column(db.String(10), nullable=True)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    def __repr__(self):
+        return "<User: {}>".format(self.id)
+
+
+class Message(db.Model):
+
+    __tablename__ = "messages"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.String(255))
+    raw = db.Column(db.Text, nullable=True)
+    priority = db.Column(db.String(10))
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user = db.relationship("User", backref="user")
+
+    def __repr__(self):
+        return "<Message: {}>".format(self.id)
+
+
+class IndexResource(Resource):
+    def get(self):
+        return {
+            "working": "OK"
+        }
+
+
+class UsersResource(Resource):
+    def get(self):
+        items = User.query.all()
+        data = []
+        for item in items:
+            data.append({
+                "id": item.id,
+                "first_name": item.first_name,
+                "last_name": item.last_name,
+                "age": item.age,
+                "location": item.location,
+                "country": item.country,
+                "created_at": item.created_at.strftime("%Y-%m-%d %H:%M:%S")
+            })
+        return data
+
+    def post(self):
+        data = request.get_json()
+        item = User(**data)
+        db.session.add(item)
+        db.session.commit()
+        return {
+            "id": item.id,
+            "first_name": item.first_name,
+            "last_name": item.last_name,
+            "age": item.age,
+            "location": item.location,
+            "country": item.country,
+            "created_at": item.created_at.strftime("%Y-%m-%d %H:%M:%S")
+        }, 201
+
+
+class UserIDResource(Resource):
+    def get(self, id):
+        item = User.query.get_or_404(1)
+        return {
+            "id": item.id,
+            "first_name": item.first_name,
+            "last_name": item.last_name,
+            "age": item.age,
+            "location": item.location,
+            "country": item.country,
+            "created_at": item.created_at.strftime("%Y-%m-%d %H:%M:%S")
+        }
+    
+    def patch(self, id):
+        item = User.query.get_or_404(1)
+        data = request.get_json()
+        item.first_name = data.get("first_name", item.first_name)
+        item.last_name = data.get("last_name", item.last_name)
+        item.age = data.get("age", item.age)
+        item.location = data.get("location", item.location)
+        item.country = data.get("country", item.country)
+        db.session.add(item)
+        db.session.commit()
+        return {
+            "id": item.id,
+            "first_name": item.first_name,
+            "last_name": item.last_name,
+            "age": item.age,
+            "location": item.location,
+            "country": item.country,
+            "created_at": item.created_at.strftime("%Y-%m-%d %H:%M:%S")
+        }
+    
+    def delete(self, id):
+        item = User.query.get_or_404(1)
+        db.session.delete(item)
+        db.session.commit()
+        return {}, 204
+
+
+api.add_resource(IndexResource, "/")
+api.add_resource(UsersResource, "/users/")
+api.add_resource(UserIDResource, "/users/<int:id>")
+
+# /messages/ GET, POST
+# /messages/<id> GET, PATCH, DELETE
