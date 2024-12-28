fccb2692bd0bb0d49c764796dc20dd7c706c2a5b
Cristhian <cristhianclx@gmail.com>
2024-12-28 16:54:48 -0500

    08

--- 08/main.py
+++ 08/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
@@ -11,6 +12,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+ma = Marshmallow(app)
+
 socketio = SocketIO(app)
 
 
@@ -21,12 +24,30 @@ class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    # importance = db.Column(db.String(10), nullable=False, default="LOW") # LOW, HIGH
-    # details # String(200)
+    importance = db.Column(db.String(10), nullable=False, default="LOW") # LOW, HIGH
+    details = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
     def __repr__(self):
         return "<Message: {}>".format(self.id)
+
+
+class MessageSchema(ma.Schema):
+    class Meta:
+        model = Message
+        fields = (
+            "id",
+            "user",
+            "content",
+            "importance",
+            "details",
+            "created_at",
+        )
+        datetimeformat = "%Y-%m-%d %H:%M:%S"
+
+
+message_schema = MessageSchema()
+messages_schema = MessageSchema(many = True)
 
 
 @app.route("/ping/")
@@ -52,8 +73,9 @@ def handle_ws_messages(data):
     item = Message(**data)
     db.session.add(item)
     db.session.commit()
+    data = message_schema.dump(item)
+    is_important = False
+    if data.get("importance", "") == "HIGH":
+        is_important = True
+    data["is_important"] = is_important
     socketio.emit("ws-messages-responses", data)
-
-
-# LABORATORIO
-# el chat que manejamos incluya los campos, importance y details
